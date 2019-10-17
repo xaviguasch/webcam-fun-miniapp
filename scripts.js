4986fd350e5f0662da67c5fde627fe500dd6dc6f
xaviguasch <xaviguasch@gmail.com>
2019-10-17 10:02:23 +0200

    added rgbSplit and redEffect color manipulations to the stream

--- scripts.js
+++ scripts.js
@@ -26,6 +26,17 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height)
+
+    // take the pixels
+    let pixels = ctx.getImageData(0, 0, width, height)
+
+    // mess with them
+    // pixels = redEffect(pixels)
+
+    pixels = rgbSplit(pixels)
+
+    // put them back
+    ctx.putImageData(pixels, 0, 0)
   }, 16)
 }
 
@@ -39,10 +50,28 @@ function takePhoto() {
   const link = document.createElement('a')
   link.href = data
   link.setAttribute('download', 'handsome')
-  link.textContent = 'Download Image'
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`
   strip.insertBefore(link, strip.firstChild)
 }
 
-video.addEventListener('canplay', paintToCanvas)
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200 // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50 // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5 // Blue
+  }
+  return pixels
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0] // RED
+    pixels.data[i + 500] = pixels.data[i + 1] // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2] // Blue
+  }
+  return pixels
+}
 
 // getVideo()
+
+video.addEventListener('canplay', paintToCanvas)
