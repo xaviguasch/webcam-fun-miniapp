83f6b74a4faf6031b4ecf5611752464dbffde324
xaviguasch <xaviguasch@gmail.com>
2019-10-17 09:38:14 +0200

    added option to take and download a photo of the webcam stream

--- scripts.js
+++ scripts.js
@@ -33,6 +33,14 @@ function takePhoto() {
   //played the sound
   snap.currentTime = 0
   snap.play()
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg')
+  const link = document.createElement('a')
+  link.href = data
+  link.setAttribute('download', 'handsome')
+  link.textContent = 'Download Image'
+  strip.insertBefore(link, strip.firstChild)
 }
 
 video.addEventListener('canplay', paintToCanvas)
